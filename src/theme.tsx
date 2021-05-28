f6ea1b33e3cd7937beed8a3c9af8358cd66fe14e
p3javier <65142395+p3javier@users.noreply.github.com>
2021-05-28 02:00:05 +0200

    Material-UI configured

---
+++ src/theme.tsx
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#f8b816",
+    },
+    secondary: {
+      main: "#e53743",
+    },
+  },
+});
+
+export default theme;
