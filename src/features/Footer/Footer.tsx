d4145b38a42e09af8fd1719170c6127c5e3ae343
p3javier <65142395+p3javier@users.noreply.github.com>
2021-06-06 18:51:56 +0200

    mobile footer added

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import FooterDesktop from "./FooterDesktop";
+import FooterMobile from "./FooterMobile";
+import Hidden from "@material-ui/core/Hidden";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -12,13 +11,9 @@ const useStyles = makeStyles((theme: Theme) =>
       backgroundColor: "#3a1313",
       color: "#fff",
       minHeight: 400,
-      fontFamily: "Roboto !important",
     },
     content: {
       padding: "3rem  2rem",
-    },
-    text: {
-      fontFamily: "Roboto",
     },
   })
 );
@@ -28,80 +23,12 @@ export default function Footer() {
   return (
     <footer className={classes.footer}>
       <Grid container spacing={4} className={classes.content}>
-        <Grid item xs={12} sm={2}>
-          <Typography variant="h6" className={classes.text}>
-            Nuestra comida
-          </Typography>
-          <List dense className={classes.text}>
-            <ListItem>
-              <ListItemText>Los más vendidos</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Menú del día</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Ofertas</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Cupones</ListItemText>
-            </ListItem>
-          </List>
-        </Grid>
-        <Grid item xs={12} sm={2}>
-          <Typography variant="h6">Sobre Nosotros</Typography>
-          <List dense>
-            <ListItem>
-              <ListItemText>Nuestra historia</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Nuestro proceso de elaboración</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Trabaja con nosotros</ListItemText>
-            </ListItem>
-          </List>
-        </Grid>
-        <Grid item xs={12} sm={2}>
-          <Typography variant="h6">Contacta con nosotros</Typography>
-          <List dense>
-            <ListItem>
-              <ListItemText>Encuentranos</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Contacta con nosotros</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Preguntas frecuentes</ListItemText>
-            </ListItem>
-          </List>
-        </Grid>
-
-        <Grid item xs={12} sm={2}>
-          <Typography variant="h6">Nutrición</Typography>
-          <List dense>
-            <ListItem>
-              <ListItemText>Información nutricional</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Alergenos</ListItemText>
-            </ListItem>
-          </List>
-          <Typography variant="h6">Políticas</Typography>
-          <List dense>
-            <ListItem>
-              <ListItemText>Política de Privacidad</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Uso de Cookies</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Nota Legal</ListItemText>
-            </ListItem>
-          </List>
-        </Grid>
-        <Grid item xs={12} sm={2}>
-          <Typography variant="h5">Redes Sociales</Typography>
-        </Grid>
+        <Hidden smDown>
+          <FooterDesktop />
+        </Hidden>
+        <Hidden smUp>
+          <FooterMobile />
+        </Hidden>
       </Grid>
     </footer>
   );
---
+++ src/features/Footer/FooterMobile.tsx
@@ -0,0 +1,143 @@
+import React from "react";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      backgroundColor: "#3a1313",
+      color: "#fff",
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+    accordion: {
+      backgroundColor: "#3a1313",
+      color: "#fff",
+    },
+  })
+);
+
+export default function FooterMobile() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Accordion className={classes.accordion}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon htmlColor="#fff" />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography variant="h6">Nuestra comida</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <List dense>
+            <ListItem>
+              <ListItemText>Los más vendidos</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText>Menú del día</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText>Ofertas</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText>Cupones</ListItemText>
+            </ListItem>
+          </List>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion className={classes.accordion}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon htmlColor="#fff" />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography variant="h6">Sobre Nosotros</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <List dense>
+            <ListItem>
+              <ListItemText>Nuestra historia</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText>Nuestro proceso de elaboración</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText>Trabaja con nosotros</ListItemText>
+            </ListItem>
+          </List>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion className={classes.accordion}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon htmlColor="#fff" />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          <Typography variant="h6">Contacta con nosotros</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <List dense>
+            <ListItem>
+              <ListItemText>Encuentranos</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText>Contacta con nosotros</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText>Preguntas frecuentes</ListItemText>
+            </ListItem>
+          </List>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion className={classes.accordion}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon htmlColor="#fff" />}
+          aria-controls="panel4-content"
+          id="panel4-header"
+        >
+          <Typography variant="h6">Nutrición</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <List dense>
+            <ListItem>
+              <ListItemText>Información nutricional</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText>Alergenos</ListItemText>
+            </ListItem>
+          </List>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion className={classes.accordion}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon htmlColor="#fff" />}
+          aria-controls="panel4-content"
+          id="panel4-header"
+        >
+          <Typography variant="h6">Políticas</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <List dense>
+            <ListItem>
+              <ListItemText>Información nutricional</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText>Alergenos</ListItemText>
+            </ListItem>
+          </List>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
