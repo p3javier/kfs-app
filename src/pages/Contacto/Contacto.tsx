77832c917f4b38101699832d3029c68fc8309250
p3javier <65142395+p3javier@users.noreply.github.com>
2022-01-10 19:00:37 +0100

    added new elements

--- src/pages/Contacto/Contacto.tsx
+++ src/pages/Contacto/Contacto.tsx
@@ -1,7 +1,30 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-//import { TextField } from "formik-material-ui";
+import { Formik, Form, Field } from "formik";
+import { TextField } from "formik-mui";
+import { Container, Button, Grid, Typography } from "@mui/material";
 import * as yup from "yup";
+
+import Box, { BoxProps } from "@mui/material/Box";
+
+function Item(props: BoxProps) {
+  const { sx, ...other } = props;
+  return (
+    <Box
+      sx={{
+        bgcolor: "primary.main",
+        color: "white",
+        p: 1,
+        m: 1,
+        borderRadius: 1,
+        textAlign: "center",
+        fontSize: "1rem",
+        fontWeight: "700",
+        ...sx,
+      }}
+      {...other}
+    />
+  );
+}
 
 const contactSchema = yup.object({
   nombre: yup.string().required("Es obligatorio"),
@@ -17,40 +40,72 @@ const contactSchema = yup.object({
 });
 const Contacto = () => {
   return (
-    <div>
-      <Formik
-        initialValues={{
-          nombre: "",
-          email: "",
-          telefono: "",
-          consulta: "",
-        }}
-        validationSchema={contactSchema}
-        onSubmit={(values) => {
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
-        <Form>
-          <label htmlFor="nombre">Nombre</label>
-          <Field name="nombre" type="text" />
-          <ErrorMessage name="nombre" />
+    <Container maxWidth="md">
+      <Typography variant="h2" component="div" gutterBottom>
+        Contacto / Reservas
+      </Typography>
 
-          <label htmlFor="email">Correo</label>
-          <Field name="email" type="email" />
-          <ErrorMessage name="email" />
-
-          <label htmlFor="telefono">Teléfono</label>
-          <Field name="telefono" type="tel" />
-          <ErrorMessage name="telefono" />
-
-          <label htmlFor="consulta">Consulta</label>
-          <Field name="consulta" type="text" />
-          <ErrorMessage name="consulta" />
-
-          <button type="submit">Enviar</button>
-        </Form>
-      </Formik>
-    </div>
+      <Box sx={{ display: "grid", gridTemplateRows: "repeat(3, 1fr)" }}>
+        <Formik
+          initialValues={{
+            nombre: "",
+            email: "",
+            telefono: "",
+            consulta: "",
+          }}
+          validationSchema={contactSchema}
+          onSubmit={(values) => {
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          <Form>
+            <Item>
+              <Field
+                color="secondary"
+                component={TextField}
+                name="nombre"
+                type="text"
+                label="nombre"
+                htmlFor="nombre"
+                fullWidth
+              />
+            </Item>
+            <Item>
+              <Field
+                component={TextField}
+                name="email"
+                type="email"
+                label="email"
+                htmlFor="email"
+                fullWidth
+              />
+            </Item>
+            <Item>
+              <Field
+                component={TextField}
+                name="telefono"
+                type="tel"
+                label="telefono"
+                htmlFor="telefono"
+                fullWidth
+              />
+            </Item>
+            <Item>
+              <Field
+                component={TextField}
+                name="consulta"
+                type="text"
+                label="consulta"
+                htmlFor="consulta"
+                multiline
+                fullWidth
+              />
+            </Item>
+            <Button type="submit">Enviar</Button>
+          </Form>
+        </Formik>
+      </Box>
+    </Container>
   );
 };
 
