39135084f045a657cd6ec3ebd2520a78d2fe38c9
p3javier <65142395+p3javier@users.noreply.github.com>
2022-02-05 09:23:50 +0100

    router removed

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -20,28 +20,26 @@ const Routes = () => {
   const classes = useStyles();
   return (
     <Grid className={classes.root}>
-      <Router>
-        <Switch>
-          <Route path="/servicios" component={Servicios} exact />
-          <Route path="/ciudades" component={Ciudades} exact />
-          <Route path="/" component={Home} exact />
-          <Route path="/contacto" component={Contacto} exact />
-          {data.ciudades.map((ciudad) => (
-            <Route
-              path={ciudad.ficha}
-              render={() => (
-                <CityPage
-                  imagen={ciudad.imagen}
-                  introduction={ciudad.pageData?.introduction}
-                  servicios={ciudad.pageData?.servicios}
-                />
-              )}
-              exact
-            />
-          ))}
-          {/*TODO: Check JSON Schema type validation compatibility with TypeScript */}
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/servicios" component={Servicios} exact />
+        <Route path="/ciudades" component={Ciudades} exact />
+        <Route path="/" component={Home} exact />
+        <Route path="/contacto" component={Contacto} exact />
+        {data.ciudades.map((ciudad) => (
+          <Route
+            path={ciudad.ficha}
+            render={() => (
+              <CityPage
+                imagen={ciudad.imagen}
+                introduction={ciudad.pageData?.introduction}
+                servicios={ciudad.pageData?.servicios}
+              />
+            )}
+            exact
+          />
+        ))}
+        {/*TODO: Check JSON Schema type validation compatibility with TypeScript */}
+      </Switch>
     </Grid>
   );
 };
