8a473e52426e3ce4131290d255e096a14f271dce
p3javier <65142395+p3javier@users.noreply.github.com>
2022-05-06 01:55:43 +0200

    updated email form

--- src/app/components/EmailSubForm/EmailSubForm.tsx
+++ src/app/components/EmailSubForm/EmailSubForm.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-import { Field } from "formik";
 import * as yup from "yup";
 
 import axios from "axios";
 import qs from "qs";
 import { useFormik } from "formik";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
+import Button, { ButtonProps } from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import { alpha, styled } from "@mui/material/styles";
+
+const CustomTextField = styled(TextField)<TextFieldProps>(({ theme }) => ({
+  margin: `${theme.spacing(2)} 0`,
+  "& label.Mui-focused": {
+    color: theme.palette.secondary.main,
+  },
+  "& .MuiFilledInput-root": {
+    backgroundColor: "#7D7D7D",
+    "& fieldset": {},
+    "&.Mui-focused": {
+      borderColor: theme.palette.secondary.main,
+    },
+    "&.MuiFilledInput-underline": {
+      textDecorationColor: theme.palette.secondary.main,
+    },
+  },
+}));
 
 const validationSchema = yup.object({
   nombre: yup.string().required("Es obligatorio"),
@@ -51,36 +69,48 @@ const EmailSubForm = () => {
       <form onSubmit={formik.handleSubmit} name="email-sub" data-netlify="true">
         <input type="hidden" name="bot-field" />
         <input type="hidden" name="form-name" />
-
-        <TextField
-          variant="filled"
-          color="primary"
-          name="nombre"
-          type="text"
-          label="nombre"
-          value={formik.values.nombre}
-          onChange={formik.handleChange}
-          error={formik.touched.nombre && Boolean(formik.errors.nombre)}
-          helperText={formik.touched.nombre && formik.errors.nombre}
-        />
-        <TextField
-          name="email"
-          type="email"
-          label="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <Button
-          size="large"
-          variant="contained"
-          color="success"
-          disableElevation
-          type="submit"
-        >
-          <Typography variant="h6"> SUSCRIBIRSE</Typography>
-        </Button>
+        <Grid container spacing={3}>
+          <Grid item>
+            <CustomTextField
+              variant="filled"
+              color="primary"
+              name="nombre"
+              type="text"
+              label="nombre"
+              value={formik.values.nombre}
+              onChange={formik.handleChange}
+              error={formik.touched.nombre && Boolean(formik.errors.nombre)}
+              helperText={formik.touched.nombre && formik.errors.nombre}
+            />
+          </Grid>
+          <Grid item>
+            <CustomTextField
+              variant="filled"
+              name="email"
+              type="email"
+              label="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              size="large"
+              variant="contained"
+              color="success"
+              disableElevation
+              type="submit"
+              sx={(theme) => ({
+                margin: `${theme.spacing(2)} 0`,
+                padding: theme.spacing(1.5),
+              })}
+            >
+              <Typography variant="h6"> SUSCRIBIRSE</Typography>
+            </Button>
+          </Grid>
+        </Grid>
       </form>
     </>
   );
